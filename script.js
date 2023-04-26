7e1f7ff2640b4d22df89eaa43d90f916cbc5302f
vramble <derpyturtle123@gmail.com>
2023-04-26 17:14:29 -0500

    trying to get putimagedata working

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ var imgWidth;
 var imgHeight;
 
 function testImage(){
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
     console.log('this function triggered!');
     var image = document.getElementById('output');
 	image.src = URL.createObjectURL(event.target.files[0]);
     image.onload=function(){
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
         ctx.drawImage(image, 0, 0);
-        ctx.canvas.width=image.width;
-        ctx.canvas.height=image.height;
         imgWidth=image.width;
         imgHeight=image.height;
-        imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+        imgData = ctx.getImageData(0, 0, imgHeight, imgWidth).data;
         console.log(imgData);
         document.getElementById("height").innerHTML=imgHeight;
         document.getElementById("width").innerHTML=imgWidth;
+        //var idata = ctx.createImageData(imgWidth, imgHeight);
+        //for(var i = 0; i < idata.data.length; i++) idata.data[i] = imgData[i];
+        //ctx.putImageData(idata,0,0);
     };
 };
